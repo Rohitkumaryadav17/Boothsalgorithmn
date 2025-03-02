28d48b943490b108c7f7a6f2d87837c77ed5c660
Rohit Kumar Yadav <your_email@example.com>
2025-03-02 15:50:32 +0530

    Updated feature or bug fix

--- BoothsAlgorithm.java
+++ BoothsAlgorithm.java
@@ -1,5 +1,7 @@
+import java.util.*;
 public class BoothsAlgorithm {
     static String binaryToDecimal(String binary) {
+
         int num = Integer.parseInt(binary, 2);
         if (binary.charAt(0) == '1') {
             num -= (1 << binary.length());
@@ -54,10 +56,16 @@ public class BoothsAlgorithm {
     }
 
     public static void main(String[] args) {
-        int multiplicand = 5;
-        int multiplier = -3;
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Multiplicand : ");
+        int a=sc.nextInt();
+        System.out.print("Enter multiplier :");
+        int b=sc.nextInt();
+        // int multiplicand = 5;
+        // int multiplier = -3;
         int bits = 8;
-        int result = boothMultiplication(multiplicand, multiplier, bits);
+        int result = boothMultiplication(a, b, bits);
         System.out.println("Result: " + result);
     }
 }
